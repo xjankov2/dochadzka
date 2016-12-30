bee2f3d47e204e79f3d8b1e70bc8725589c2977f
jankovj <jaroslav.jankovic89@gmail.com>
2016-12-30 13:01:26 +0100

    Fix Ivana Jankovicova Person

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,7 +9,7 @@ insert into record_type (CODE, DESCRIPTION, HOURS_TYPE, ATTEND_TYPE) values ('OT
 
 -- PERSON
 insert into person (ID, FIRST_NAME, LAST_NAME, PREFIX_DEGREE, SUFFIX_DEGREE, VACATION_COUNT)  values (1, 'Roman', 'Devera', '', '', 20);
-insert into person (ID, FIRST_NAME, LAST_NAME, PREFIX_DEGREE, SUFFIX_DEGREE, VACATION_COUNT)  values (2, 'Ivana', 'Janko', '', '', 20);
+insert into person (ID, FIRST_NAME, LAST_NAME, PREFIX_DEGREE, SUFFIX_DEGREE, VACATION_COUNT)  values (2, 'Ivana', 'Jankovičová', '', '', 20);
 insert into person (ID, FIRST_NAME, LAST_NAME, PREFIX_DEGREE, SUFFIX_DEGREE, VACATION_COUNT)  values (3, 'Vierka', 'Bendulová', '', '', 20);
 insert into person (ID, FIRST_NAME, LAST_NAME, PREFIX_DEGREE, SUFFIX_DEGREE, VACATION_COUNT)  values (4, 'Ján', 'Kratochvíla', '', '', 20);
 insert into person (ID, FIRST_NAME, LAST_NAME, PREFIX_DEGREE, SUFFIX_DEGREE, VACATION_COUNT)  values (5, 'Katka', 'Bachanová', '', '', 20);
